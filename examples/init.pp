2a02b7f231da76204df1376a65894b5bdb88e6f0
fdammeke <fabian.dammekens@cegeka.be>
2012-10-19 14:57:11 +0200

    refined example

--- examples/init.pp
+++ examples/init.pp
@@ -11,7 +11,7 @@ tomcat::instance { 'tomcat00 instance':
   tomcat_instance_uid      => '1101',
   tomcat_instance_password => '$1$JOOZyS5c$JDJq9SdMWVZi8IRT/Lh2H1',
   tomcat_version           => '6.0.32',
-  tomcat_options           => { 'testvalue' => 'valuex', 'testvalue 3' => 'valuey' }
+  tomcat_options           => { 'SERVER_PORT' => '8050', 'HTTP_PORT' => '8080', 'AJP_PORT' => '8010' }
 }
 
 tomcat::instance { 'tomcat01 instance':
@@ -21,7 +21,7 @@ tomcat::instance { 'tomcat01 instance':
   tomcat_instance_uid      => '1102',
   tomcat_instance_password => '$1$8WHwrwUf$JmAywFDPdE4CwyjaNHn010',
   tomcat_version           => '6.0.32',
-  tomcat_options           => { 'testvalue' => 'valuex', 'testvalue 3' => 'valuey' }
+  tomcat_options           => { 'SERVER_PORT' => '8051', 'HTTP_PORT' => '8081', 'AJP_PORT' => '8011' }
 }
 
 #tomcat::conf::setenv { 'setting tomcat env':
