81e6451cd8888748e8762f0a213f4851c4c26bf9
fdammeke <fabian.dammekens@cegeka.be>
2012-10-19 16:21:46 +0200

    Added repo needed for cegeka-tomcat

--- manifests/redhat/server.pp
+++ manifests/redhat/server.pp
@@ -1,12 +1,26 @@
 define tomcat::redhat::server($tomcat_version) {
 
+  include yum
+
   $tomcat_major_version = regsubst($tomcat_version, '^(\d+)\.(\d+)\.(\d+)$','\1')
 
   debug("tomcat_major_version=${tomcat_major_version}")
   debug("tomcat_version=${tomcat_version}")
 
-  package { "cegeka-tomcat$tomcat_major_version":
+  package { "cegeka-tomcat${tomcat_major_version}":
     ensure => $tomcat_version,
+  }
+
+  yum::repo { 'cegeka-public':
+    ensure          => present,
+    scheme          => 'https',
+    host            => 'yum.cegeka.be',
+    repo_root       => 'cegeka-public',
+    descr           => 'Cegeka public RPM repo',
+    enabled         => '1',
+    sslverify       => 'True',
+    sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt',
+    metadata_expire => '5m',
   }
 
 }
