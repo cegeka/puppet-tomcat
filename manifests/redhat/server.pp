edba38e322fe62f2e206fbbebb732324521aa92e
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-10-19 08:07:40 +0200

    Some cleanup & fixes

--- manifests/redhat/server.pp
+++ manifests/redhat/server.pp
@@ -2,6 +2,9 @@ class tomcat::redhat::server($tomcat_version) {
 
   $tomcat_major_version = regsubst($tomcat_version, '^(\d+)\.(\d+)\.(\d+)$','\1')
 
+  debug("tomcat_major_version=${tomcat_major_version}")
+  debug("tomcat_version=${tomcat_version}")
+
   package { "cegeka-tomcat$tomcat_major_version":
     ensure => $tomcat_version,
   }
