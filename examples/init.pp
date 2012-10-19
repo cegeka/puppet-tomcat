d7a22490183190966375bbdffd4f5c349b950ead
fdammeke <fabian.dammekens@cegeka.be>
2012-10-19 17:58:25 +0200

    Added tomcat options, sysconfig fixes, initd export var

--- examples/init.pp
+++ examples/init.pp
@@ -1,41 +1,33 @@
-$tomcat_version = '6.0.32'
+# Environment variables
+$tomcat_version = '6.0.32-4'
 $tomcat_instance_root_dir = '/data'
 
-tomcat::server { 'tomcat server':
+# Install tomcat $version
+tomcat::server { "tomcat-${tomcat_version}":
   tomcat_version => $tomcat_version,
 }
 
-tomcat::instance { 'tomcat00 instance':
-  tomcat_instance_root_dir => $tomcat_instance_root_dir,
-  tomcat_instance_number   => '00',
-  tomcat_instance_gid      => '1001',
-  tomcat_instance_uid      => '1101',
-  tomcat_instance_password => '$1$JOOZyS5c$JDJq9SdMWVZi8IRT/Lh2H1',
-  tomcat_version           => $tomcat_version,
-  tomcat_options           => { 'SERVER_PORT' => '8050', 'HTTP_PORT' => '8080', 'AJP_PORT' => '8010' }
+# Create a tomcat instance $name
+tomcat::instance { 'tomcat00':
+  tomcat_instance_root_dir    => $tomcat_instance_root_dir,
+  tomcat_instance_number      => '00',
+  tomcat_instance_gid         => '1001',
+  tomcat_instance_uid         => '1101',
+  tomcat_instance_password    => '$1$JOOZyS5c$JDJq9SdMWVZi8IRT/Lh2H1',
+  tomcat_version              => $tomcat_version,
+  tomcat_options              => {
+      'SERVER_PORT'           => '8050',
+      'HTTP_PORT'             => '8080',
+      'AJP_PORT'              => '8010',
+      'TOMCAT_INSTANCE_PROPS' => '"-Xmx256m -XX:MaxPermSize=128m\ 
+        -Dtn.tomcat.server.port=$SERVER_PORT -Dtn.tomcat.connector.http.port=$HTTP_PORT\
+        -Dtn.tomcat.connector.ajp.port=$AJP_PORT\
+        -Dtn.tomcat.connector.ajp.maxThreads=200\
+        -Dtn.tomcat.engine.jvmRoute=${TOMCAT_NAME}${TOMCAT_NUMBER}"'
+  }
 }
 
-tomcat::instance { 'tomcat01 instance':
-  tomcat_instance_root_dir => $tomcat_instance_root_dir,
-  tomcat_instance_number   => '01',
-  tomcat_instance_gid      => '1002',
-  tomcat_instance_uid      => '1102',
-  tomcat_instance_password => '$1$8WHwrwUf$JmAywFDPdE4CwyjaNHn010',
-  tomcat_version           => $tomcat_version,
-  tomcat_options           => { 'SERVER_PORT' => '8051', 'HTTP_PORT' => '8081', 'AJP_PORT' => '8011' }
-}
-
-#tomcat::conf::setenv { 'setting tomcat env':
-#  java_options => [
-#    '-Xms512m',
-#    '-Xmx1024m',
-#    '-XX:PermSize=256m',
-#    '-XX:MaxPermSize=512m',
-#    '-Denv.ENVIRONMENT=test'
-#  ],
-
-#}
-
+# Configure settings for the tomcat instance
 tomcat::instance::role { 'adding foo role':
   tomcat_instance_root_dir => $tomcat_instance_root_dir,
   tomcat_instance_number   => '00',
