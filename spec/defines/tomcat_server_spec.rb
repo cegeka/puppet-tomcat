7df446efbc0d8ea92145c140872b9b822ffdbdfb
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-10-24 11:04:45 +0200

    Fix spec test by adding precondition for yum resource

--- spec/defines/tomcat_server_spec.rb
+++ spec/defines/tomcat_server_spec.rb
@@ -7,6 +7,8 @@ describe 'tomcat::server' do
     let (:facts) { { :operatingsystem => 'redhat' } }
     let(:params) { { :tomcat_version => '6.0.32-4' } }
 
+    let(:pre_condition) { "define yum::repo($scheme, $host, $repo_root, $ensure = present, $descr = undef, $enabled = '1', $gpgcheck = '1', $sslverify = 'False', $sslcacert = undef, $sslclientcert = undef, $sslclientkey = undef, $metadata_expire = undef) {}" "class yum {}" }
+
     it { should contain_package('httplog').with(
         :ensure => 'present'
       )
