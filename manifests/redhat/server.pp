0fba4a86eb95797d331890f79076dd083a4ea102
fdammeke <fabian.dammekens@cegeka.be>
2012-10-24 12:41:32 +0200

    Only add cegeka-public if not defined

--- manifests/redhat/server.pp
+++ manifests/redhat/server.pp
@@ -9,16 +9,18 @@ define tomcat::redhat::server($tomcat_version) {
 
   Yum::Repo['cegeka-public'] -> Package['httplog'] -> Package["cegeka-tomcat${tomcat_major_version}"]
 
-  yum::repo { 'cegeka-public':
-    ensure          => present,
-    scheme          => 'https',
-    host            => 'yum.cegeka.be',
-    repo_root       => 'cegeka-public',
-    descr           => 'Cegeka public RPM repo',
-    enabled         => '1',
-    sslverify       => 'True',
-    sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt',
-    metadata_expire => '5m',
+  if ! defined(Yum::Repo['cegeka-public']) {
+    yum::repo { 'cegeka-public':
+      ensure          => present,
+      scheme          => 'https',
+      host            => 'yum.cegeka.be',
+      repo_root       => 'cegeka-public',
+      descr           => 'Cegeka public RPM repo',
+      enabled         => '1',
+      sslverify       => 'True',
+      sslcacert       => '/etc/pki/tls/certs/ca-bundle.crt',
+      metadata_expire => '5m',
+    }
   }
 
   package { "cegeka-tomcat${tomcat_major_version}":
