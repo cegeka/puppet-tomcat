3fcc309be719426ac24b1795a73ee856aaddabd5
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-10-24 15:17:57 +0200

    Implementation for setenv in server/instance

--- manifests/redhat/setenv.pp
+++ manifests/redhat/setenv.pp
@@ -1,13 +1,31 @@
-define tomcat::redhat::setenv($java_options=undef) {
+define tomcat::redhat::setenv($instance=true, $tomcat_instance_root_dir=undef, $tomcat_instance_number=undef, $java_options=undef) {
 
-  #include tomcat
+  case $instance {
+    true:
+    {
+      file { "${tomcat_instance_root_dir}/tomcat${tomcat_instance_number}/bin/setenv.sh":
+        ensure  => file,
+        content => template("${module_name}/bin/setenv.sh.erb"),
+        owner   => "tomcat${tomcat_instance_number}",
+        group   => "tomcat${tomcat_instance_number}",
+        mode    => '0754',
+      }
+    }
+    false:
+    {
+      if $::tomcat_installation_dir == undef {
+        fail("parameter $::tomcat_installation_dir is undefined")
+      }
 
-  file { "${::tomcat_installation_dir}/bin/setenv.sh":
-    ensure  => file,
-    content => template("${module_name}/bin/setenv.sh.erb"),
-    owner   => 'tomcat',
-    group   => 'tomcat',
-    mode    => '0754',
+      file { "${::tomcat_installation_dir}/bin/setenv.sh":
+        ensure  => file,
+        content => template("${module_name}/bin/setenv.sh.erb"),
+        owner   => 'tomcat',
+        group   => 'tomcat',
+        mode    => '0754',
+      }
+    }
+    default: { fail("Tomcat::Redhat::Setenv[${title}]: parameter instance must be a boolean")  }
   }
 
 }
--- manifests/server/setenv.pp
+++ manifests/server/setenv.pp
@@ -3,6 +3,7 @@ define tomcat::server::setenv($java_options=undef) {
   case $::operatingsystem {
     redhat, centos: {
       tomcat::redhat::setenv { $name:
+        instance     => false,
         java_options => $java_options
       }
     }
