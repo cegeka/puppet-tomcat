474793fe73c68ba60414611f15bcc07f33a96188
fdammeke <fabian.dammekens@cegeka.be>
2013-03-20 16:34:45 +0100

    One should add the tomcat instance number when setting env properties

--- manifests/redhat/setenv.pp
+++ manifests/redhat/setenv.pp
@@ -1,12 +1,20 @@
-define tomcat::redhat::setenv($java_options=undef) {
+define tomcat::redhat::setenv($ensure,
+  $tomcat_instance_root_dir,
+  $tomcat_instance_number,
+  $java_options
+) {
 
-  #include tomcat
+  $tomcat_instance_name = "tomcat${tomcat_instance_number}"
+  $real_tomcat_instance_dir = "${tomcat_instance_root_dir}/${tomcat_instance_name}"
 
-  file { "${::tomcat_installation_dir}/bin/setenv.sh":
-    ensure  => file,
+  File {
+    owner => $tomcat_instance_name,
+    group => $tomcat_instance_name
+  }
+
+  file { "${real_tomcat_instance_dir}/bin/setenv.sh":
+    ensure  => $ensure,
     content => template("${module_name}/bin/setenv.sh.erb"),
-    owner   => 'tomcat',
-    group   => 'tomcat',
     mode    => '0754',
   }
 
