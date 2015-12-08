df25734928eee418010ac3935aa10264a6ca0bda
fdammeke <fabian.dammekens@cegeka.be>
2015-12-08 13:33:22 +0100

    style fix

--- manifests/instance/jaas.pp
+++ manifests/instance/jaas.pp
@@ -1,8 +1,12 @@
-define tomcat::instance::jaas($tomcat_instance_root_dir, $tomcat_instance_number, $loginconf=undef) {
+define tomcat::instance::jaas (
+  $tomcat_instance_root_dir,
+  $tomcat_instance_number,
+  $loginconf=undef
+) {
 
   case $::operatingsystem {
-    redhat, centos: {
-      tomcat::redhat::jaas { $name:
+    'redhat', 'centos': {
+      tomcat::redhat::jaas { $name :
         tomcat_instance_root_dir => $tomcat_instance_root_dir,
         tomcat_instance_number   => $tomcat_instance_number,
         loginconf                => $loginconf
