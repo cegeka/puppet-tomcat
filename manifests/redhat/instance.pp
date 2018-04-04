fd569896114c959ce62b98f947d7857cc360c8b5
andreias <andrei.asan@cegeka.com>
2018-04-04 15:05:20 +0200

    posibility to define umask for tomcat + enable on 01 02 05 rentaweb

--- manifests/redhat/instance.pp
+++ manifests/redhat/instance.pp
@@ -18,6 +18,8 @@ define tomcat::redhat::instance(
     $tomcat_ssl_connector_enabled=false,
     $tomcat_ssl_keystore_file=undef,
     $tomcat_ssl_keystore_password=undef,
+    $tomcat_use_umask=false,
+    $tomcat_umask='0002'
     $java_home='/usr/java/latest'
   ) {
 
@@ -149,13 +151,22 @@ define tomcat::redhat::instance(
       }
     }
   }
-
-  file { "/etc/init.d/${tomcat_instance_name}":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    content => template("${module_name}/etc/init.d/tomcat.erb"),
+  if $tomcat_use_umask {
+    file { "/etc/init.d/${tomcat_instance_name}":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template("${module_name}/etc/init.d/tomcat_umask.erb"),
+    }
+  } else {
+    file { "/etc/init.d/${tomcat_instance_name}":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template("${module_name}/etc/init.d/tomcat.erb"),
+    }
   }
 
   file { "/etc/sysconfig/${tomcat_instance_name}":
